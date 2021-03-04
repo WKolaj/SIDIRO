a625c06f57ac2300f2576b9eb3fa384e475f701e
WKolaj <WKolaj@gmail.com>
2021-03-04 14:09:07 +0100

    Fixed issue with app crash when credentials are invalid

--- src/startup/appData.ts
+++ src/startup/appData.ts
@@ -2,27 +2,31 @@ import { MindSphereAppsManager } from "../classes/MindSphereApp/MindSphereAppsMa
 import logger from "../logger/logger";
 
 export default async function() {
-  //Fetching all app data to the manager
-  let appManager = MindSphereAppsManager.getInstance();
+  try {
+    //Fetching all app data to the manager
+    let appManager = MindSphereAppsManager.getInstance();
 
-  let allAppsAssets = await appManager.getAllAppAssets();
-
-  await Promise.all(
-    allAppsAssets.map(
-      (asset) =>
-        new Promise(async (resolve: Function, reject: Function) => {
-          try {
-            await appManager.fetchAppFromAsset(asset);
-            return resolve();
-          } catch (err) {
-            logger.error(`error while initializing app ${asset.name}`);
-            logger.error(err.message, err);
-            //Resolving in order not to throw in case of an error
-            return resolve();
-          }
-        })
-    )
-  );
-
-  logger.info("applications data initialized");
+    let allAppsAssets = await appManager.getAllAppAssets();
+    console.log(allAppsAssets);
+    await Promise.all(
+      allAppsAssets.map(
+        (asset) =>
+          new Promise(async (resolve: Function, reject: Function) => {
+            try {
+              await appManager.fetchAppFromAsset(asset);
+              return resolve();
+            } catch (err) {
+              logger.error(`error while initializing app ${asset.name}`);
+              logger.error(err.message, err);
+              //Resolving in order not to throw in case of an error
+              return resolve();
+            }
+          })
+      )
+    );
+    logger.info("applications data initialized");
+  } catch (err) {
+    logger.error(err.message, err);
+    logger.info("applications data initialization failure !");
+  }
 }
